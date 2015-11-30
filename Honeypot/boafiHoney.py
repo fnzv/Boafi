7eaba0d672d04e0744dd5d6e413d33fd5218d525
Yessou Sami <yessou.sami@gmail.com>
2015-11-30 19:49:16 +0100

    Update boafiHoney.py

--- Honeypot/boafiHoney.py
+++ Honeypot/boafiHoney.py
@@ -1,6 +1,3 @@
-#!/usr/bin/python
-import os,time,argparse
-
 #####  BoafiHoney.py
 ###  - Creates a dns black hole and starts apache server (to run a Captive Portal or any web site)
 ##   - Dependencies : bind9,apache, (working hostspot.. dhcp & hostapd)
@@ -12,8 +9,9 @@ import os,time,argparse
 ###  Project Boafi
 ###  Tested on Raspberry pi 2(Raspian) with ALFA AWUS052NH
 
+#!/usr/bin/python
 
-
+import os,time,argparse
 
 parser = argparse.ArgumentParser()
 
@@ -27,7 +25,7 @@ parser.add_argument('-blackhole', action='store_true', default=False,
 # Stealth honeypot  ---> Captive portal ----> Internet
 #parser.add_argument('-stealth', action='store_true', default=False,
 #                   dest='loadall',
-#                  help='Start Captive Portal only one time')
+#                  help='Start All the services and uses the ip address as Tor Gateway')
 
 
 
@@ -70,21 +68,63 @@ bind_cfg="""$TTL    604800
 .       IN      A       """+ipaddr+"""
 *.      IN      A       """+ipaddr
 
+print bind_cfg
+
+#this minimal hostapd conf will be used if none is found
+hostapd_cfg="""ddns-update-style none;
+default-lease-time 600;
+max-lease-time 7200;
+subnet 192.168.42.0 netmask 255.255.255.0{
+ range 192.168.42.10 192.168.42.255;
+ option broadcast-address 192.168.42.255;
+ option routers 192.168.42.1;
+ default-lease-time 7000;
+ max-lease-time 9000;
+ option domain-name-servers 192.168.42.1,192.168.42.1;
+}
+"""
 
 
-cfg_check=os.path.exists("/etc/bind/db.catchall") #Check if the dns configuration exists 
+cfg_check=os.path.exists("/etc/bind/db.catchall")
+cfg2_check=os.path.exists("/etc/hostapd/hostapdhole.conf")
+if not(cfg2_check):
+  file= open("/etc/hostapd/hostapdhole.conf")
+  file.write(hostapd_cfg)
+  file.close()
 
 
 
-if(results.blackhole and not cfg_check):
-        # Write the dns black hole configuration
-        file = open("/etc/bind/db.catchall", "wb")
-        file.write(bind_cfg)
-        file.close()
-        #Start services
-        os.popen("service apache2 start") # You need to have your files of the captive portal on your /var/www
-        os.popen("service bind9 start") # Starting bind dns for black hole
-        print "Started Captive Portal"
+file=open("/etc/bind/named.conf.local","r")
+named="""zone "." {
+        type master;
+        file "/etc/bind/db.catchall";
+};"""
+
+
+if(named in file.read()):
+        print "IT's OK"
+else:
+        file2=open("/etc/bind/named.conf.local","a")
+        file2.write(named)
+        file2.close()
+        print "Wrote named cfg"
+
+
+
+
+if(results.blackhole):
+        if not (cfg_check):
+                file = open("/etc/bind/db.catchall", "wb")
+                file.write(bind_cfg)
+                file.close()
+        os.popen("service apache2 start")
+        os.popen("service bind9 start")
+        os.popen("service hostapd stop")
+        os.popen("nohup hostapd /etc/hostapd/hostapdhole.conf >/dev/null 2>&1 &")
+        #Started hostapd with the dnsblack hole configuration
+
+
+        print "started hostapd"
 
 ##TODO
 ## Stealth captive portal
