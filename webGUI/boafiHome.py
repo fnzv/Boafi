057889da3d5ccf7fb2b15f539936e177a2c02eaf
Yessou Sami <yessou.sami@gmail.com>
2015-12-13 16:23:42 +0100

    Update boafiHome.py

--- webGUI/boafiHome.py
+++ webGUI/boafiHome.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python
 ####
 #### Script for Home\Index page 
 #### 
@@ -7,6 +6,8 @@
 #### -..
 
 ##
+#!/usr/bin/python
+
 import os,time,argparse
 
 
@@ -20,17 +21,20 @@ parser.add_argument('-wifi', action='store_true', dest='wifi', default=False,
 parser.add_argument('-killCap', action='store_true', dest='killme', default=False,
                     help='Kill mac address gathering and shuts monitor interface')
 
+parser.add_argument('-AutoCap', action='store_true', dest='autocap', default=False,
+                    help='Adds to startup the AutoCapture function of BoafiCap')
+
+parser.add_argument('-NoAutoCap', action='store_true', dest='noautocap', default=False,
+                    help='Removes BoafiCap from start up')
 
 
 results = parser.parse_args()
 
 
-
 wifi=results.wifi
 killme=results.killme
-
-
-
+autocap=results.autocap
+noautocap=results.noautocap
 
 
 if(wifi):
@@ -42,7 +46,7 @@ if(wifi):
                  wifinets=os.popen("""iw dev """+wlan+""" scan ap-force | grep 'SSID' | awk '{ $1=""; print }'  """).read()
 
                  print wifinets
-                 #print on screen || write on file || write on db || or ?
+                 #other options : print on screen || write on file || write on db || or ?
                  file.write(wifinets)
         file.close()
 
@@ -50,5 +54,18 @@ if(wifi):
 if (killme):
         os.popen("killall airodump-ng")
         os.popen("ifconfig mon0 down")
-        
+        os.popen("ifconfig mon1 down")
 
+
+if(autocap):
+        boafiCap="@reboot sudo python "+(os.popen('pwd').read()).strip()+'/BoafiCap.py -csv -pcap -o AutoCapture'
+        #Must be on the same directory of BoafiCap
+        print boafiCap
+        ## MUST be root to edit crontab  ##
+        os.popen("sudo echo '"+boafiCap+"' >> /var/spool/cron/crontabs/root")
+
+if(noautocap):
+        boafiCap="@reboot sudo python "+(os.popen('pwd').read()).strip()+'/BoafiCap.py -csv -pcap -o AutoCapture'
+        crontab="/var/spool/cron/crontabs/root"
+        filecron=open(crontab).read()
+        open(crontab,"w").write(filecron.replace(boafiCap,""))
